b2b61101bb5206653e9ce07b96a1331381b98a1d
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-04-04 14:42:33 -0400

    commit tree 3a1454a59263cac9138cfa267375ad7040433352

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+shout('Hello' = "hello");
+
+ 
+function whisper(string) {
+    return string.toLowerCase();
+}
+whisper('HELLO' = "hello");
+
+function logShout(string) {
+    console.log(string.toUpperCase())
+    const spy = expect.spyOn('log').andCallThrough
+  }
+  logShout('hello');
+   
+  function logWhisper(string) {
+    console.log(string.toLowerCase())
+  }
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string == 'hello')
+    return "I can't hear you!"
+    else if (string === 'HELLO')
+    return "YES INDEED!"
+    else (string == "Let's have dinner together!")
+    return "I would love to!"
+
+}
+
+sayHiToHeadphonedRoommate()
